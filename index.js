8e5f05d5397322f0a5dcb22cf029f0f3eade41d9
Sean <shonkelli@gmail.com>
2019-04-05 17:23:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.begin = beginningLocation
+    this.end = endingLocation
+    this.eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+
+  }
+  blocksTravelled(){
+    let verticalDistance =
+    Math.abs(parseInt(this.begin["vertical"]) - parseInt(this.end["vertical"]))
+    let horizontalDistance = Math.abs(this.eastWest.indexOf(this.begin["horizontal"]) - this.eastWest.indexOf(this.end["horizontal"]))
+    return horizontalDistance + verticalDistance
+  }
+
+  estimatedTime(peak){
+    let coefficient
+    if (peak) {
+      coefficient = 2
+    } else {
+      coefficient = 3
+    }
+    return this.blocksTravelled() / coefficient
+  }
+}
