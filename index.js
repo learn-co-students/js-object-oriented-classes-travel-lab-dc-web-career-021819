33107defaf2583a9b192ad64068ffd0c423ae7a6
Mickey <micksheridan.24@gmail.com>
2019-04-06 10:55:17 -0400

    submit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - parseInt(this.startDate.getFullYear());
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ["1st Avenue", "2nd Avenue", "3rd Avenue", "Lexington Avenue", "Park", "Madison Avenue", "5th Avenue"];
+
+    return this.calcDistance(eastWest);
+  }
+
+  calcDistance(eastWest) {
+    const horizontalDistance = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+    const verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    return horizontalDistance + verticalDistance;
+  }
+
+  estimatedTime(peak = false) {
+    const b = this.blocksTravelled();
+
+    return peak ? b / 2 : b / 3;
+  }
+}
